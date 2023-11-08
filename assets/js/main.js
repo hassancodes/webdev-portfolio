087ed96ae6a32d560df737bb5d56bc0d37430c58
Muhammad hassan <hassanhanjra900@gmail.com>
2023-11-07 23:12:38 -0500

    update: cursor

--- assets/js/main.js
+++ assets/js/main.js
@@ -261,3 +261,26 @@
 
 
 })(jQuery);
+
+
+
+
+// custom cursor: 
+
+
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+window.addEventListener("mousemove", function (e) {
+const posX = e.clientX;
+const posY = e.clientY;
+cursorDot.style.left = `${posX}px`;
+cursorDot.style.top= `${posY}px`;
+
+cursorOutline.animate({
+    left: `${posX}px`,
+    top:`${posY}px`
+},{duration:500, fill:"forwards"});
+
+
+
+});
